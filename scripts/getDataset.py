4277344085c6077975f5e276b53942139299bafc
Chengjie <chengjiepeng90@gmail.com>
2025-11-22 22:44:12 +0100

    Added Random Forest Initial Model and GridSearch

---
+++ scripts/getDataset.py
@@ -0,0 +1,19 @@
+
+import kagglehub
+import shutil
+import os
+
+# Descargar el dataset
+path = kagglehub.dataset_download("leonardopena/top-spotify-songs-from-20102019-by-year")
+
+# path normalmente apunta a un archivo comprimido o CSV; si es ZIP, hay que extraerlo
+# Supongamos que es un CSV directamente
+
+# Ruta de destino en el directorio actual con el nombre deseado
+dest_path = os.path.join(os.getcwd(), "../dataset")
+
+# Mover/renombrar el archivo descargado
+shutil.move(path, dest_path)
+
+print("Dataset guardado en:", dest_path)
+
